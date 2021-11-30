3f7a092fe645a9327b09f4c311ea9c08eaf45fed
gsombras <gsombras@alumnos.nebrija.es>
2021-12-01 00:04:09 +0100

    Initial commit

---
+++ main.py
@@ -0,0 +1,8 @@
+class Car:
+    def __init__(self, ancho):
+
+    def print(self):
+        print("Ancho: ", self.ancho)
+        print("Rodadura: ", self.rodadura)
+        print("Diámetro: ", self.diametro)
+        print("Presión: ", self.presion)
